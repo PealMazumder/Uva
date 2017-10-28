52580067398baad361a78063fbbbfd9ad0bcb5ab
Peal Mazumder <33184874+PealMazumder@users.noreply.github.com>
2017-10-29 02:42:02 +0600

    Create 100 - The 3n + 1 problem uva.c

---
+++ 100 - The 3n + 1 problem uva.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+int max(int p,int e);
+int main()
+{
+    int input1,input2,result;
+    while(scanf("%d %d",&input1,&input2)!=EOF)
+    {
+        if(input1<input2)
+        {
+            result=max(input1,input2);
+        }
+        else
+        {
+            result=max(input2,input1);
+        }
+
+        printf("%d %d %d\n",input1,input2,result);
+    }
+    return 0;
+}
+int max(int p,int e)
+{
+    int i,j, count=0,Max=0;
+    for(i=p;i<=e;i++)
+    {
+        for(j=i;;)
+        {
+            count++;
+            if(j==1)
+            {
+                break;
+            }
+            else if(j%2==0)
+            {
+                j=j/2;
+                continue;
+            }
+            else if(j%2!=0)
+            {
+                j=j*3+1;
+                continue;
+            }
+        }
+        if(count>Max)
+        {
+            Max=count;
+
+        }
+        count=0;
+    }
+    return Max;
+}
+
