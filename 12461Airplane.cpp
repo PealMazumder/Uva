110dd67192bf8e76a18d0b492a5384e3f7693475
Peal Mazumder <33184874+PealMazumder@users.noreply.github.com>
2018-03-25 18:48:20 +0600

    Create 12461Airplane.cpp

---
+++ 12461Airplane.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for(int i=1;;i++)
+    {
+        int input;
+        cin>>input;
+        if(input==0)
+            break;
+        else
+            cout<<1<<"/"<<2<<endl;
+    }
+    return 0;
+}
