a2290b6386cc6e7a793ebede9a3f99e16d251932
Peal Mazumder <33184874+PealMazumder@users.noreply.github.com>
2019-01-18 00:55:50 +0600

    Stack

---
+++ 673 - Parentheses Balance.cpp
@@ -0,0 +1,56 @@
+/*
+	http://www.shafaetsplanet.com/planetcoding/?p=2342
+	Above article may help you to understand deque.
+*/
+#include<bits/stdc++.h>
+#define nl "\n"
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    cin.ignore();
+    
+    
+    while(n--)
+    {
+    	string s;
+    	getline(cin,s);
+    	stack <char> b;
+    	
+    
+	    for(int i=0;i<s.length();i++)
+	   	{
+	   		if(s[i]=='('||s[i]=='[')
+	   			b.push(s[i]);
+	   		else if(s[i]==']')
+    		{  
+				if(b.empty()||b.top()!='[')
+	    		{
+	   				b.push(s[i]);
+	   				break;
+				} 
+				else
+	        		if(b.top()=='[')
+	        			b.pop();				
+			}
+			else if(s[i]==')')
+	   		{
+	   			if(b.empty()||b.top()!='(')
+	   			{
+	    			b.push(s[i]);
+	   				break;
+				}
+				else
+	    			if(b.top()=='(')
+	    				b.pop();	
+			}
+		}
+		if(b.empty())
+			cout<<"Yes"<<nl;
+		else
+			cout<<"No"<<nl;
+	}
+    return 0;
+}
+
