bc6b9511b0574271e798190dcbb5c71c65af06bc
Peal Mazumder <33184874+PealMazumder@users.noreply.github.com>
2019-03-13 12:40:26 +0600

    Two Pointer

--- 1121 - Subsequence.cpp
+++ 1121 - Subsequence.cpp
@@ -8,7 +8,7 @@ int main()
     //ios::sync_with_stdio(false); cin.tie(0); cout.tie(NULL);
     ll n,s;
     freopen("input.txt", "r", stdin);
-freopen("output.txt", "w", stdout);
+	freopen("output.txt", "w", stdout);
     while(scanf("%lld%lld",&n,&s)==2)
     {
     	vector<ll>v(n+1);
